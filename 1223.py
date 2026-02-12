90845894806f026bd34a67bf3a61fcc1b9239582
heisenburger3 <rinat.rinat.1302@gmail.com>
2026-02-12 16:39:46 +0300

    3 задача

--- 1223.py
+++ 1223.py
@@ -13,10 +13,12 @@ z = 0
 
 class GameView(arcade.Window):
     def setup(self):
-        self.get_image(z)
+        self.get_image(z, lat, lon)
         self.z = z
+        self.lon = lon
+        self.lat = lat
 
-    def request(self, z):
+    def request(self, z, lat, lon):
         server_address = 'https://static-maps.yandex.ru/v1?'
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
         params = {
@@ -28,12 +30,24 @@ class GameView(arcade.Window):
         return response
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.PAGEUP and self.z < 21:
+        if key == arcade.key.LEFT:
+            self.lon = str(float(self.lon) - 0.001)
+            self.get_image(self.z, self.lat, self.lon)
+        elif key == arcade.key.RIGHT:
+            self.lon = str(float(self.lon) + 0.001)
+            self.get_image(self.z, self.lat, self.lon)
+        elif key == arcade.key.UP:
+            self.lat = str(float(self.lat) + 0.001)
+            self.get_image(self.z, self.lat, self.lon)
+        elif key == arcade.key.DOWN:
+            self.lat = str(float(self.lat) - 0.001)
+            self.get_image(self.z, self.lat, self.lon)
+        elif key == arcade.key.PAGEUP and self.z < 21:
             self.z += 1
-            self.get_image(self.z)
+            self.get_image(self.z, lat, lon)
         elif key == arcade.key.PAGEDOWN and self.z > 0:
             self.z -= 1
-            self.get_image(self.z)
+            self.get_image(self.z, lat, lon)
 
     def on_draw(self):
         self.clear()
@@ -48,8 +62,8 @@ class GameView(arcade.Window):
             ),
         )
 
-    def get_image(self, z):
-        response = self.request(z)
+    def get_image(self, z, lat, lon):
+        response = self.request(z, lat, lon)
         with open(MAP_FILE, "wb") as file:
             file.write(response.content)
 
